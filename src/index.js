b37f05d12f94e570e6fb0921347590b1d5371041
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-04-16 20:57:34 +0200

    install Firebase and Reactfire

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FirebaseAppProvider } from 'reactfire';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMhOFj__TM9qd18HG-JL4JOJWC4gtfCQY",
+  authDomain: "mvp-app-b233e.firebaseapp.com",
+  projectId: "mvp-app-b233e",
+  storageBucket: "mvp-app-b233e.appspot.com",
+  messagingSenderId: "784383986018",
+  appId: "1:784383986018:web:bf188429b9a24884ce843b"
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
     <App />
-  </React.StrictMode>
+  </FirebaseAppProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function

4fed6bba4cb20dcaf7ec12fc2aafc68e182cc47b
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-04-17 01:36:59 +0200

    make Auth work

--- src/Auth.js
+++ src/Auth.js
@@ -1,21 +1,51 @@
 import { useState } from 'react';
 import SignIn from './SignIn';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useAuth, useSigninCheck } from 'reactfire';
+
 
 const Auth = () => {
+
+    const signOut = (auth) => auth.signOut().then(() => {
+        console.log('User has signed out.');
+    });
+
+    const signIn = (auth, user) => {
+       return signInWithEmailAndPassword(auth, user.email, user.password);
+    }
+
+    const auth = useAuth();
 
     const [user, setUser] = useState({
         email: "",
         password: ""
       });
     
-      const handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         console.log(user);
         event.preventDefault();
+        signIn(auth, user)
+        .then((data) => {
+            console.log(`${data.user.displayName} has signed in.`);
+        })
+        .catch((error) => {
+            console.error(error.message, error.code);
+        });
       };
+    
+    const { status, data: signinResult } = useSigninCheck();
 
-    return ( 
-        <SignIn onSubmit={handleSubmit} value={user} setValue={setUser}/> 
-    );
+    if (status === 'loading') {
+        return <div>...loading</div>;
+    }
+    
+    const signedIn = signinResult.signedIn;
+    
+      if (signedIn === true) {
+        return <button onClick={() => signOut(auth)}>sign out</button>;
+      } else {
+        return <SignIn onSubmit={handleSubmit} value={user} setValue={setUser}/> ;
+      }
 }
  
 export default Auth;
